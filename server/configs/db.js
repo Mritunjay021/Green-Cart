b7abed3e53a7a29535b2e56cab00786389e3ec6e
Mritunjay021 <mrityurk21@gmail.com>
2026-05-09 01:50:06 +0530

    fix vercel deployment

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const  connectDB = async() =>{
     try {
         mongoose.connection.on('connected',()=>console.log("DB Connected"))
+        mongoose.connection.on('error', (err) => console.log("DB Error:", err));
 
         await(mongoose.connect(`${process.env.MONGODB_URI}/greencart`))
     }
